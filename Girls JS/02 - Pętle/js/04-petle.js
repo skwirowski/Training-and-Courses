c5af3eb38c56749924b9e5225abc7d75dbc38ee7
Paweł Skwirowski <pskwirowski@gmail.com>
2018-06-06 21:59:03 +0200

    Add Girls JS Loops exercise 04
    - added loop exercise - reverse array

---
+++ Girls JS/02 - Pętle/js/04-petle.js
@@ -0,0 +1,52 @@
+// 4.
+// Stwórz tablicę, a następnie
+// przy pomocą pętli stwórz tablicę zawierającą
+// te same elementy w odwrotnej kolejności.
+// Wyświetl je w alercie.
+//
+
+let genericNumberArray = [
+    " one",
+    " two",
+    " three",
+    " four",
+    " five",
+    " six",
+    " seven",
+    " eight",
+    " nine",
+    " ten"
+];
+
+let genericLetterArray = [
+    " A",
+    " B",
+    " C",
+    " D",
+    " E",
+    " F",
+    " G",
+    " H",
+    " I",
+    " J"
+];
+
+// Podejście z użyciem metody reverse()
+
+genericNumberArray.reverse();
+
+for (let i = 0; i < genericNumberArray.length; i++) {
+    console.log(genericNumberArray[i]);
+}
+
+// Podejście alternatywne
+
+function reverseArray(array) {
+    let reversedArray = [];
+    for (let i = array.length - 1; i >= 0; i--) {
+        reversedArray.push(array[i]);
+    }
+    return reversedArray;
+}
+
+alert(reverseArray(genericLetterArray));
