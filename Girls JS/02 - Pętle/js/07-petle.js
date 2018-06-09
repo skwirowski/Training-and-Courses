38ba395b7513bd570c6838664d64efa4e885b9da
Paweł Skwirowski <pskwirowski@gmail.com>
2018-06-09 19:24:08 +0200

    Add GirlsJS loop exercise 07 initial commit

---
+++ Girls JS/02 - Pętle/js/07-petle.js
@@ -0,0 +1,9 @@
+// 7.
+// stwórz tablicę zawierającą 3 obiekty.
+// Wyświetl na stronie wszystkie elementy
+// wszystkich obiektów, zaznaczając którego obiektu
+// elementy są wyświetlone.
+// Funkcji nie wywołuj w konsoli
+//
+
+
