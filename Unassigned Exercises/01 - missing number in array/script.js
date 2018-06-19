6fc8dc2c28124ef942d4d9ba20e2fcff778f14da
Paweł Skwirowski <pskwirowski@gmail.com>
2018-06-19 23:23:35 +0200

    Add Unassigned Exercises folder
    - added ReadMe file
    - added exercise 01 - Find the Missing Number in a given Array

---
+++ Unassigned Exercises/01 - missing number in array/script.js
@@ -0,0 +1,17 @@
+let numbers1 = [3, 8, 7, 9, 6, 4, 12, 5, 14, 19, 16, 18, 11, 10, 13, 17, 20];
+let numbers2 = [3, 8, 1, 7, 9, 6, 4, 12, 5, 14, 19, 16, 18, 11, 10, 13, 17, 0, 20, 15];
+
+function missingNumbersInArray(array) {
+    let missingNumber;
+    let sortedArray = array.sort((a, b) => a - b);
+    console.log(sortedArray);
+
+    for (let i = sortedArray[0]; i <= array.length; i++) {
+        if (array.indexOf(i) === -1) {
+            missingNumber = i;
+        }
+    }
+    return missingNumber;
+}
+console.log(missingNumbersInArray(numbers1));
+console.log(missingNumbersInArray(numbers2));
---
+++ Unassigned Exercises/ReadMe.md
@@ -0,0 +1,4 @@
+# Unassigned Exercises in JavaScript
+================================
+## Exercise 01
+### Find the Missing Number in a given Array
