67ecd660b599f134a2394e9b3fc1c34a2151a966
Paweł Skwirowski <pskwirowski@gmail.com>
2018-07-05 20:25:37 +0200

    Add ReactJS/Redux main course exercises
    - State management
      * application setup
        + added initial styles
        + added PhotoWall and Photo components
        + passed array of data (PhotoWall) mapped to render up three photo
    instances (Photo)
        + rendered pictures from data array
        + added remove button
      * functional components - change class components (PhotoWall, Photo,
    Title) into functional components
      * state management - added initial state of the component

---
+++ Udemy/photowall/src/Components/PhotoWall.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Photo from './Photo';
+
+function PhotoWall(props) {
+    return(
+        <div className="photo-grid">
+            {props.posts.map((post, index) => <Photo key={index} post={post}/>)}
+        </div>
+    )
+}
+export default PhotoWall
