2efe01cab2135cb9aef1698d9d1c57469d5ceb41
Paweł Skwirowski <pskwirowski@gmail.com>
2018-07-05 21:39:26 +0200

    Add ReactJS/Redux main course exercises - add remove photo functionality
    - State management - added remove photo button functionality

--- Udemy/photowall/src/Components/Main.js
+++ Udemy/photowall/src/Components/Main.js
@@ -21,13 +21,22 @@ class Main extends Component {
                 description: "On a vacation!",
                 imageLink: "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg"
             }]
-        }
+        };
+        this.removePhoto = this.removePhoto.bind(this);
     }
+
+    removePhoto(postRemoved) {
+        console.log(postRemoved.description);
+        this.setState(state => ({
+            posts: state.posts.filter(post => post !== postRemoved)
+        }));
+    }
+
     render() {
         return(
             <div>
                 <Title title={"PhotoWall"}/>
-                <PhotoWall posts={this.state.posts}/>
+                <PhotoWall posts={this.state.posts} onRemovePhoto={this.removePhoto} />
             </div>
         )
     }
--- Udemy/photowall/src/Components/Photo.js
+++ Udemy/photowall/src/Components/Photo.js
@@ -12,7 +12,10 @@ function Photo(props) {
                 <p> {post.description} </p>
             </figcaption>
             <div className="button-container">
-                <button className="remove-button"> Remove </button>
+                <button className="remove-button"
+                        onClick={() => {props.onRemovePhoto(post)}}>
+                Remove
+                </button>
             </div>
         </figure>
     )
