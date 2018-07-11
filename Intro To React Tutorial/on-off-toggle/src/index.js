569024dd8c888226c1051085f9738b48a9be9745
Paweł Skwirowski <pskwirowski@gmail.com>
2018-07-11 13:48:25 +0200

    Add "ON" and "OFF" Toggle exercise from React Tutorial - events handling
    - added "ON" and "OFF" Toggle component - lets toggle between on/off
    button state
    - updated ReadMe file with proper exercise description

---
+++ Intro To React Tutorial/on-off-toggle/src/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isToggleOn: true
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+
+  componentDidUpdate() {
+    console.log(this.state);
+  }
+
+  render() {
+    return(
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+        </button>
+    );
+  }
+
+}
+
+ReactDOM.render(
+  <Toggle />,
+  document.getElementById('root')
+);
