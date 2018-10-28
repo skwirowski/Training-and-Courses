18d83f80fe92bc7542ffd0ce68090452cd8c8715
Paweł Skwirowski <pskwirowski@gmail.com>
2018-10-28 18:16:46 +0100

    Add Sort and Star exercise

--- CodeWars Exercises/script.js
+++ CodeWars Exercises/script.js
@@ -1,9 +1,23 @@
-// Example string
-const exampleString = '_nnnnnnn_n__n______nn__nn_nnn';
-function bump(x) {
-  // Regular Expression
-  const regExp = /n/g;
-  const bumpsNumber = (x.match(regExp) || []).length;
-  return (bumpsNumber <= 15) ? 'Woohoo!' : 'Car Dead';
+// Example array
+const exampleArray = ['take', 'over', 'the', 'world', 'maybe', 'who', 'knows', 'bitcoin', 'perhaps'];
+const sortAlphabetically = arr => arr.sort((a, b) => {
+  if (a < b) return -1;
+  if (a > b) return 1;
+  return 0;
+});
+
+const addAsterisks = (str) => {
+  let newString = '';
+  for (let i = 0; i < str.length; i += 1) {
+    const newVal = `${str.charAt(i)}***`;
+    newString += newVal;
+  }
+  return newString.slice(0, -3);
+};
+
+function twoSort(s) {
+  const sortedArray = sortAlphabetically(s);
+  return addAsterisks(sortedArray[0]);
 }
-console.log(bump(exampleString));
+console.log(sortAlphabetically(exampleArray));
+console.log(twoSort(exampleArray));
