4ba5215f2b76726eae18b34a455d21bd0319bbe9
Paweł Skwirowski <pskwirowski@gmail.com>
2018-10-28 16:06:27 +0100

    Add Bumps in the Road Exercise

--- CodeWars Exercises/script.js
+++ CodeWars Exercises/script.js
@@ -1,6 +1,9 @@
-function michaelPays(costs) {
-  if (costs < 5) return +costs.toFixed(2);
-  if ((1 / 3 * costs) > 10) return +(costs - 10).toFixed(2);
-  return +(2 / 3 * costs).toFixed(2);
+// Example string
+const exampleString = '_nnnnnnn_n__n______nn__nn_nnn';
+function bump(x) {
+  // Regular Expression
+  const regExp = /n/g;
+  const bumpsNumber = (x.match(regExp) || []).length;
+  return (bumpsNumber <= 15) ? 'Woohoo!' : 'Car Dead';
 }
-console.log(michaelPays(6.75));
+console.log(bump(exampleString));
