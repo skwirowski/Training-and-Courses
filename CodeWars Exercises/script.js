6781649ea2a914eaabbab8827f69e4c835ebd727
Paweł Skwirowski <pskwirowski@gmail.com>
2018-10-28 19:34:57 +0100

    Add Are the Numbers in Order? exercise

--- CodeWars Exercises/script.js
+++ CodeWars Exercises/script.js
@@ -1,23 +1,8 @@
 // Example array
-const exampleArray = ['take', 'over', 'the', 'world', 'maybe', 'who', 'knows', 'bitcoin', 'perhaps'];
-const sortAlphabetically = arr => arr.sort((a, b) => {
-  if (a < b) return -1;
-  if (a > b) return 1;
-  return 0;
-});
+const exampleArray = [1, 6, 10, 18, 19, 19, 20];
 
-const addAsterisks = (str) => {
-  let newString = '';
-  for (let i = 0; i < str.length; i += 1) {
-    const newVal = `${str.charAt(i)}***`;
-    newString += newVal;
-  }
-  return newString.slice(0, -3);
-};
-
-function twoSort(s) {
-  const sortedArray = sortAlphabetically(s);
-  return addAsterisks(sortedArray[0]);
+function inAscOrder(arr) {
+  const sortedArray = () => arr.sort((a, b) => a - b);
+  return arr.join(',') === sortedArray().join(',');
 }
-console.log(sortAlphabetically(exampleArray));
-console.log(twoSort(exampleArray));
+console.log(inAscOrder(exampleArray));
