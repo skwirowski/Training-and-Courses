f5745c264cc34c174eb60c9f0586ad005904538b
Paweł Skwirowski <pskwirowski@gmail.com>
2018-11-06 22:58:58 +0100

    Add Cat and Mouse - Easy Version exercise

--- CodeWars Exercises/script.js
+++ CodeWars Exercises/script.js
@@ -1,11 +1,5 @@
-// Example array
-const exArr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
-function diagonalSum(matrix) {
-  const flatArr = matrix.reduce((total, amount) => total.concat(amount));
-  let sum = 0;
-  for (let i = 0; i <= flatArr.length; i += matrix.length + 1) {
-    sum += flatArr[i];
-  }
-  return sum;
+function catMouse(x) {
+  const regExp = /[.]/g;
+  return (x.match(regExp) || []).length <= 3 ? 'Caught!' : 'Escaped!';
 }
-console.log(diagonalSum(exArr));
+console.log(catMouse(''));
