c491c28a2a7807f7c56651d4b2a25c94d9848464
Paweł Skwirowski <pskwirowski@gmail.com>
2018-11-06 22:03:02 +0100

    Add Find Sum of Diagonals exercise

--- CodeWars Exercises/script.js
+++ CodeWars Exercises/script.js
@@ -1,28 +1,11 @@
-// Example strings
-const exStr1 = 'katas';
-const exStr2 = 'steak';
-
-const scramble = (str1, str2) => {
-  let canRearrange;
-  for (let i = 0; i < str2.length; i += 1) {
-    const char = str2.charAt(i);
-    const regExp = new RegExp(char, 'g');
-    /*
-    * Compare how many times each character occurs in the base string (str2 - string to check for)
-    * and in compared string (str1 - string to be checked). If character occurs in str1 at least
-    * as many times as in str2, then loop iteration returns canRearrange = true. Otherwise loop
-    * iteration returns canRearrange = false and immediately breaks loop (jump out of the loop).
-    */
-    const occur1 = (str1.match(regExp) || []).length;
-    const occur2 = (str2.match(regExp) || []).length;
-    if (occur1 >= occur2) {
-      canRearrange = true;
-    } else {
-      canRearrange = false;
-      break;
-    }
+// Example array
+const exArr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+function diagonalSum(matrix) {
+  const flatArr = matrix.reduce((total, amount) => total.concat(amount));
+  let sum = 0;
+  for (let i = 0; i <= flatArr.length; i += matrix.length + 1) {
+    sum += flatArr[i];
   }
-  return canRearrange;
-};
-
-console.log(scramble(exStr1, exStr2));
+  return sum;
+}
+console.log(diagonalSum(exArr));
