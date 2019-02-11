3d67dea51a05a01d9baee879e97d87df785018b8
Paweł Skwirowski <pskwirowski@gmail.com>
2019-02-11 21:44:57 +0100

    Add exercise #28 Digital cypher vol. 3

--- CodeWars Exercises/script.js
+++ CodeWars Exercises/script.js
@@ -1,12 +1,27 @@
+const exStr = 'scout';
 const exCode = [20, 12, 18, 30, 21];
-const exKey = 1939;
 
-const decode = (code, n) => {
-  const key = String(n);
-  const arr = [...code].map((item, index) => item - Number(key[index % key.length]) + 96);
-  return arr
-    .map(item => String.fromCharCode(item))
-    .join('');
+const findTheKeyArr = (message, code) => {
+  return [...message].map((item, index) => code[index] - item.charCodeAt(0) + 96);
 };
 
-console.log(decode(exCode, exKey));
+const checkKey = (code, message, key) => {
+  let resultKey;
+  for (let i = 0; i <= message.length; i += 1) {
+    const keyArr = key.slice(0, i);
+    const word = String
+      .fromCharCode(
+        ...code.map((item, index) => item - Number(keyArr[index % keyArr.length]) + 96)
+      );
+    resultKey = keyArr;
+    if (word === message) { break; }
+  }
+  return Number(resultKey.join(''));
+};
+
+const findTheKey = (message, code) => {
+  const keyArr = findTheKeyArr(message, code);
+  return checkKey(code, message, keyArr);
+};
+
+console.log(findTheKey(exStr, exCode));
