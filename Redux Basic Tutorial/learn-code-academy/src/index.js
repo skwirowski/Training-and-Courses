f47164e0ca1425d2f5dd478c6f7314d7e2303bda
Paweł Skwirowski <pskwirowski@gmail.com>
2019-07-13 16:42:32 +0200

    Add redux tutorial lesson #4 changes

--- Redux Basic Tutorial/learn-code-academy/src/index.js
+++ Redux Basic Tutorial/learn-code-academy/src/index.js
@@ -1,33 +1,33 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
+import { combineReducers, createStore } from 'redux';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
-
-import { createStore } from 'redux';
-
-const reducer = function(state, action) {
-    if(action.type === "INC") {
-        return state + action.payload;
-    }
-    if(action.type === "DEC") {
-        return state - action.payload;
+const userReducer = (state = {}, action) => {
+    switch (action.type) {
+        case 'CHANGE_NAME': {
+            state = { ...state, name: action.payload };
+            break;
+        }
+        case 'CHANGE_AGE': {
+            state = { ...state, age: action.payload };
+            break;
+        }
     }
     return state;
 };
 
-const store = createStore(reducer, 0);
+const tweetsReducer = (state = [], action) => {
+    return state;
+};
+
+const reducers = combineReducers({
+    user: userReducer,
+    tweets: tweetsReducer,
+})
+
+const store = createStore(reducers);
 
 store.subscribe(() => {
-    console.log("store changed", store.getState())
+    console.log('store changed', store.getState())
 });
 
-store.dispatch({type: "INC", payload: 5});
-store.dispatch({type: "DEC", payload: 3});
+store.dispatch({ type: 'CHANGE_NAME', payload: 'Paul' });
+store.dispatch({ type: 'CHANGE_AGE', payload: 33 });
