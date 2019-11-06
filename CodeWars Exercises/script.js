d5ce07b48f8955f0bbfff9f2df4e9b8dcb64659c
Paweł Skwirowski <pskwirowski@gmail.com>
2019-11-06 14:17:31 +0100

    Add exercise #32 new solution using JavaScript embedded methods (it's shorter now)

--- CodeWars Exercises/script.js
+++ CodeWars Exercises/script.js
@@ -1,22 +1,17 @@
 function correctPolishLetters(string) {
-  const diacriticsArray = string.split('');
-  const noDiacriticsArray = [];
   const lettersWithDiacriticalMarks = 'ĄąĆćĘęŁłŃńÓóŚśŻżŹź';
   const lettersCharacters = 'AaCcEeLlNnOoSsZzZz';
 
-  for (let i = 0; i < diacriticsArray.length; i += 1) {
-    const diacriticalMarkIndex = lettersWithDiacriticalMarks.indexOf(
-      diacriticsArray[i],
-    );
+  const newArr = string.split('').map(item => {
+    const diacriticalMarkIndex = lettersWithDiacriticalMarks.indexOf(item);
 
     if (diacriticalMarkIndex === -1) {
-      noDiacriticsArray[i] = diacriticsArray[i];
-    } else {
-      noDiacriticsArray[i] = lettersCharacters.substr(diacriticalMarkIndex, 1);
+      return item;
     }
-  }
+    return lettersCharacters.substr(diacriticalMarkIndex, 1);
+  });
 
-  return noDiacriticsArray.join('');
+  return newArr.join('');
 }
 
 console.log(correctPolishLetters('ĄąĆćĘęŁłŃńÓóŚśŻżŹźAA'));
