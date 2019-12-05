5138f0e3ccda4d400059c1ef14a4dffc0929c92f
Paweł Skwirowski <pskwirowski@gmail.com>
2019-12-05 14:02:58 +0100

    Add programming exercise uncommited files

--- CodeWars Exercises/script.js
+++ CodeWars Exercises/script.js
@@ -1,11 +1,22 @@
-const str = "ĄąĆćĘęŁłŃńÓóŚśŻżŹźAA"
+function maxSequence(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
 
-function correctPolishLetters(string) {
-  return string
-    .normalize('NFD')
-    .replace(/[\u0300-\u036f]/g, '')
-    .replace(/\u0141/g, 'L')
-    .replace(/\u0142/g, 'l');
+  let maxSoFar = 0;
+  let maxEndingHere = 0;
+
+  for (let i = 0; i < arr.length; i += 1) {
+    maxEndingHere += arr[i];
+    if (maxEndingHere < 0) {
+      maxEndingHere = 0;
+    }
+    if (maxSoFar < maxEndingHere) {
+      maxSoFar = maxEndingHere;
+    }
+  }
+  return maxSoFar;
 }
 
-console.log(correctPolishLetters(str));
+console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
+// console.log(maxSequence([]));
