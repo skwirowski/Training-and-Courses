42ec719a9548672dac9acafd0a041a9b736de70e
Pawel Skwirowski <pawel.skwirowski@tieto.com>
2020-05-06 14:14:15 +0200

    Add CodeWars Exercise #36 responsible drinking

--- CodeWars Exercises/script.js
+++ CodeWars Exercises/script.js
@@ -1,22 +1,43 @@
-function maxSequence(arr) {
-  if (arr.length === 0) {
-    return 0;
-  }
+const exampleOne = '2 glasses of wine and 1 shot';
+const exampleTwo = '1 beer';
+const exapmleThree = '1 shot, 5 beers, 2 shots, 1 glass of wine, 1 beer';
 
-  let maxSoFar = 0;
-  let maxEndingHere = 0;
+const hydrate = (s) => {
+  const drinksQuantity = s.match(/[1-9]/g).reduce((tot, curr) => tot + Number(curr), 0);
+  return `${drinksQuantity} ${drinksQuantity > 1 ? 'glasses' : 'glass'} of water`;
+};
 
-  for (let i = 0; i < arr.length; i += 1) {
-    maxEndingHere += arr[i];
-    if (maxEndingHere < 0) {
-      maxEndingHere = 0;
-    }
-    if (maxSoFar < maxEndingHere) {
-      maxSoFar = maxEndingHere;
-    }
-  }
-  return maxSoFar;
+function hydrate(s) {
+  //* Arrange
+  const digitsRegEx = /[1-9]/g;
+  const createArrayOfMatchedStrings = (string, regularEx) => string.match(regularEx);
+  const transformStringToNumber = (string) => Number(string);
+  const changeArrayOfStringsToArrayOfNumbers = (array, transformationCallback) => {
+    return array.map((item) => transformationCallback(item));
+  };
+  const sumOfArrayItems = (array) => array.reduce((total, current) => total + current, 0);
+  const getResultDependingOnCondition = (condition, ifTrue, ifFalse) =>
+    condition ? ifTrue : ifFalse;
+
+  //* Act
+  const getDrinksFromDescription = createArrayOfMatchedStrings(s, digitsRegEx);
+  const transformDrinksStringsToDigits = changeArrayOfStringsToArrayOfNumbers(
+    getDrinksFromDescription,
+    transformStringToNumber,
+  );
+  const totalDrinksQuantity = sumOfArrayItems(transformDrinksStringsToDigits);
+  const wordRightForm = getResultDependingOnCondition(
+    totalDrinksQuantity === 1,
+    'glass',
+    'glasses',
+  );
+
+  //* Resolve
+  const getHydrationRecommendation = `${totalDrinksQuantity} ${wordRightForm} of water`;
+
+  return getHydrationRecommendation;
 }
 
-console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
-// console.log(maxSequence([]));
+console.log(hydrate(exampleOne));
+console.log(hydrate(exampleTwo));
+console.log(hydrate(exapmleThree));
