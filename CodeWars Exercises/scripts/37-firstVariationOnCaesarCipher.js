f8ef485eb9c6744d38c73abf7db6ce44a01d49c4
Pawel Skwirowski <pawel.skwirowski@tieto.com>
2020-07-17 23:40:04 +0200

    CodeWars Exercises complete setting up jasmine-karma test environment configuration and create first tests
     - setup jasmine-karma configuration
     - add karma-webpack configuration
     - add package.json scripts for runing tests
     - start testing Caesar Cipher functions

--- CodeWars Exercises/scripts/37-firstVariationOnCaesarCipher.js
+++ CodeWars Exercises/scripts/37-firstVariationOnCaesarCipher.js
@@ -1,9 +1,62 @@
+//* Helper data
+
+/**
+ * English alphabet letters for ciphering use
+ */
+const alphabetChars = 'abcdefghijklmnopqrstuvwxyz';
+
+//* Helper functions
+
+/**
+ * Transforms whole string to uppercase
+ * @param { string } lowerCaseString
+ */
 const transformStringToUpperCase = (lowerCaseString) =>
   lowerCaseString
     .split('')
     .map((char) => char.toUpperCase())
     .join('');
 
-console.log(transformStringToUpperCase('something'));
+/**
+ * Slices specified number of characters from the beginning of the given string and places them at the end
+ * @param { string } string
+ * @param { number } shift - number of letters to shift
+ */
+const shiftCharacters = (string, shift) => {
+  const fixedStringSlice = string.slice(shift);
+  const shiftedStringSlice = string.slice(0, shift);
 
-export default transformStringToUpperCase;
+  return fixedStringSlice + shiftedStringSlice;
+};
+
+const findCharacterIndex = (char, string) => string.indexOf(char);
+
+//* Main functions
+
+const switchCharacters = (inputString, baseString, shift) => {
+  const upperCaseBaseString = transformStringToUpperCase(baseString);
+  const iterations = inputString.length;
+  const maxShift = baseString.length - 1;
+  let switchedString = '';
+  let shiftIncrement = shift;
+
+  for (let i = 0; i < iterations; i += 1) {
+    const modifier = shiftCharacters(baseString, shiftIncrement);
+    const lowerCaseCheck = findCharacterIndex(inputString[i], baseString);
+    const upperCaseCheck = findCharacterIndex(inputString[i], upperCaseBaseString);
+
+    if (lowerCaseCheck !== -1) {
+      switchedString += modifier[lowerCaseCheck];
+    } else if (upperCaseCheck !== -1) {
+      switchedString += transformStringToUpperCase(modifier)[upperCaseCheck];
+    } else {
+      switchedString += inputString[i];
+    }
+
+    shiftIncrement >= maxShift ? (shiftIncrement = 0) : (shiftIncrement += 1);
+  }
+
+  return switchedString;
+};
+
+export default { alphabetChars, transformStringToUpperCase, shiftCharacters, findCharacterIndex, switchCharacters };
--- CodeWars Exercises/test/37-firstVariationOnCaesarCipher.spec.js
+++ CodeWars Exercises/test/37-firstVariationOnCaesarCipher.spec.js
@@ -1,13 +1,91 @@
-import transformStringToUpperCase from 'scripts/37-firstVariationOnCaesarCipher';
+import caesarCipherFunctions from 'scripts/37-firstVariationOnCaesarCipher';
+
+const {
+  alphabetChars,
+  transformStringToUpperCase,
+  shiftCharacters,
+  findCharacterIndex,
+  switchCharacters,
+} = caesarCipherFunctions;
 
 describe('First Variation On Caesar Cipher', () => {
-  describe('transformStringToUpperCase function');
+  describe('transformStringToUpperCase function', () => {
+    it('it should transform given string to uppercase', () => {
+      const testString = 'hello from another world';
+      const testFunction = transformStringToUpperCase(testString);
 
-  it('it should transform given string to uppercase', () => {
-    const testString = 'hello from another world';
+      expect(testFunction).toBe('HELLO FROM ANOTHER WORLD');
+    });
+  });
 
-    const testFunction = transformStringToUpperCase(testString);
+  describe('shiftCharacters function', () => {
+    it('it should slice specified number of characters from given string and place them at the end', () => {
+      const testString = 'abcdefghij';
+      const testShiftSize = 5;
+      const testFunction = shiftCharacters(testString, testShiftSize);
 
-    expect(testFunction).toBe('HELLO FROM ANOTHER WORLD');
+      expect(testFunction).toBe('fghijabcde');
+    });
+
+    it('it should slice specified number of characters from given string and place them at the end', () => {
+      const testString = '0123456789';
+      const testShiftSize = 2;
+      const testFunction = shiftCharacters(testString, testShiftSize);
+
+      expect(testFunction).toBe('2345678901');
+    });
+
+    it('it should slice specified number of characters from given string and place them at the end', () => {
+      const testString = 'klmnopqrst';
+      const testShiftSize = 0;
+      const testFunction = shiftCharacters(testString, testShiftSize);
+
+      expect(testFunction).toBe('klmnopqrst');
+    });
+
+    it('it should slice specified number of characters from given string and place them at the end', () => {
+      const testString = 'uwxyzabcde';
+      const testShiftSize = 10;
+      const testFunction = shiftCharacters(testString, testShiftSize);
+
+      expect(testFunction).toBe('uwxyzabcde');
+    });
+  });
+
+  describe('findCharacterIndex function', () => {
+    it('it should find index of first occurrence of a specified character in given string', () => {
+      const testCharacter = 'a';
+      const testString = 'I was wondering if after all these years';
+      const testFunction = findCharacterIndex(testCharacter, testString);
+
+      expect(testFunction).toBe(3);
+    });
+
+    it('it should find index of first occurrence of a specified character in given string', () => {
+      const testCharacter = 'f';
+      const testString = 'I was wondering if after all these years';
+      const testFunction = findCharacterIndex(testCharacter, testString);
+
+      expect(testFunction).toBe(17);
+    });
+
+    it('it should find index of first occurrence of a specified character in given string', () => {
+      const testCharacter = 'x';
+      const testString = 'I was wondering if after all these years';
+      const testFunction = findCharacterIndex(testCharacter, testString);
+
+      expect(testFunction).toBe(-1);
+    });
+  });
+
+  describe('switchCharacters function', () => {
+    it('it should perform ciphering given string by shifting each character by shift value increasing by 1 each iteration', () => {
+      const testString = 'I should have known that you would have a perfect answer for me!!!';
+      const testBaseString = alphabetChars;
+      const testShiftSize = 1;
+      const testFunction = switchCharacters(testString, testBaseString, testShiftSize);
+
+      expect(testFunction).toBe('J vltasl rlhr zdfog odxr ypw atasl rlhr p gwkzzyq zntyhv lvz wp!!!');
+    });
   });
 });
